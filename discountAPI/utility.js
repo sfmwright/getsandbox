5720f39ed8272e01b43ebd21b51e21d8000aa2da
Sandbox Sync <support@getsandbox.com>
2017-09-12 16:33:07 +0000

    Changes

--- discountAPI/utility.js
+++ discountAPI/utility.js
@@ -11,12 +11,6 @@ exports.uuidv4 = function () {
 };
 
 exports.txnToken = function () {
-//  return 'xxxxxxxxx'.replace(/[xy]/g, function(c) {
-//    var r = Math.random() * 10 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-//    return v.toString(16);
-//  });
-
-
   return 'xxxxxxxxx'.replace(/[xy]/g, function(c) {
     return Math.random() * 10 | 0;
   });
