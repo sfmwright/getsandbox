33aaf49154d55d8601780fbdf8e1e7ffd46e94f4
Sandbox Sync <support@getsandbox.com>
2017-09-12 15:31:27 +0000

    Changes

--- discountAPI/main.js
+++ discountAPI/main.js
@@ -86,3 +86,19 @@ Sandbox.define('/Merchantware/ws/RetailTransaction/v4/Credit.asmx','POST', funct
     // Send the response body.
     res.render('postMerchantwareWsRetailtransactionV4Credit_asmx');
 })
+
+Sandbox.soap('/','', function(req, res) {
+    // Check the request, make sure it is a compatible type, covers both SOAP 1.1 and 1.2
+    if (!req.is('text/xml') && !req.is('application/xml') && !req.is('application/soap')) {
+        return res.send(400, 'Invalid content type, expected application/soap+xml');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/soap+xml');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.render('');
+})
