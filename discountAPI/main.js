a6f4065d527ba4a2f70e908af8d30b06378a8395
Sandbox Sync <support@getsandbox.com>
2017-09-12 15:29:22 +0000

    Changes

--- discountAPI/main.js
+++ discountAPI/main.js
@@ -69,18 +69,3 @@ function processPayment(req, res) {
     state.transactions = _.reject(state.transactions, { 'transportKey': req.query.TransportKey });
 }
 
-Sandbox.soap('/Merchantware/ws/RetailTransaction/v4/Credit.asmx', '', function(req, res) {
-    // Check the request, make sure it is a compatible type, covers both SOAP 1.1 and 1.2
-    if (!req.is('text/xml') && !req.is('application/xml') && !req.is('application/soap')) {
-        return res.send(400, 'Invalid content type, expected application/soap+xml');
-    }
-    
-    // Set the type of response, sets the content type.
-    res.type('application/soap+xml');
-    
-    // Set the status code of the response.
-    res.status(200);
-    
-    // Send the response body.
-    res.render('Refund');
-})
